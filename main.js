d807f24886a946002ea606b9e1fcf30eb5d3f1f5
asr <93777349+HulkKaBaap2@users.noreply.github.com>
2022-03-09 16:35:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function startClassification() 
+{
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/XnseiP92s/model.json',modelReady);
+
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
+function gotResults(error,results){
+    if (error){
+        console.log(error)
+
+    }
+    else{
+        console.log(results)
+random_number_r = Math.floor(Math.random()*255)+1;
+random_number_g = Math.floor(Math.random()*255)+1;
+random_number_b= Math.floor(Math.random()*255)+1;
+document.getElementById("result_label").innerHTML =  'this voice is of: '+
+results[0].label;
+document.getElementById("result_count").innerHTML =  'detected dog :  detected cat'+
+results[0].confidence.toFixed(2);
+
+document.getElementById("result_count").style.color="rgb("
++random_number_r + "," +random_number_g + "," +random_number_r+")";
+
+document.getElementById("result_label").style.color="rgb("
++random_number_r + "," +random_number_g + "," +random_number_r+")";
+img=document.getElementById('listening')
+
+if(results[0].label=="dog"){
+    img.src="bark bark.gif"
+   
+}
+
+
+
+else if(results[0].label=="cat"){
+    img.src="cat meow.gif"
+   
+}
+    }
+}
+
